4e0856bb274a6884ffa5db6e853373623b20b0bb
Colson <colsoncalifornication@gmail.com>
2024-05-06 10:51:48 +0545

    modify underlying cache: refetch newly added posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 // useQuery - Allows to get data
 // useMutation - Allows to change data
@@ -9,6 +9,10 @@ const POSTS = [
 
 function App() {
   console.log(POSTS);
+  // This is just going to return the thing that we created on main.jsx but
+  // it does have some nice functions on it
+  // i.e const queryClient = new QueryClient()
+  const queryClient = useQueryClient();
 
   const postsQuery = useQuery({
     // queryKey is key that uniquely identifies this query
@@ -35,10 +39,22 @@ function App() {
 
   const newPostMutation = useMutation({
     // Same, mutationFn expects to return a promise
+    // This mutation is changing our underlying data for this queryKey i.e. ['posts']
+    // So what we need to really do is, we need to setup onSuccess
+    // onSuccess is whenever we have successful data, what we want to do.
+    // In our case, we want to invalidate the query we have there i.e queryKey: ['posts']
+    //
     mutationFn: (title) => {
       return wait(1000).then(() =>
         POSTS.push({ id: crypto.randomUUID(), title }),
       );
+    },
+    onSuccess: () => {
+      // what we want to do is, we want to invalidate our 'posts' query
+      // so whenever we have a successful mutation when we add a new post,
+      // we want to invalidate our old post so now if we click add new button,
+      // and we wait for 1 sec, we can see it refetch that data immediately for us
+      queryClient.invalidateQueries(['posts']);
     },
   });
 
@@ -52,7 +68,10 @@ function App() {
       {postsQuery.data.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
-      <button onClick={() => newPostMutation.mutate('New Post')}>
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate('New Post')}
+      >
         Add New
       </button>
     </div>
